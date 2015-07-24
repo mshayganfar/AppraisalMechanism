4bfb3e1e9e9d0f0e972158217c4de0e9ec504e4f
mohammad <mshayganfar@wpi.edu>
2015-07-24 15:00:25 -0400

    Just created a new project for Appraisal mechanism.

---
+++ src/AppraisalMechanism.java
@@ -0,0 +1,19 @@
+/*
+ * Copyright (c) 2015, Mahni Shayganfar
+ * 
+ * This program uses the JESS (Java Expert System Shell) as a rule-based 
+ * system and to introduce different types of mental states (e.g., beliefs).
+ * It also uses the JUNG (Java Universal Network/Graph Framework) to create
+ * the corresponding graphs according to mental state.
+ * 
+ * This program is designed to compute values of different appraisal variables. 
+ * 
+ */
+
+public class AppraisalMechanism {
+
+	public static void main(String[] args) {
+
+	}
+
+}
