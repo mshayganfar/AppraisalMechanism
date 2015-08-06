d6be536e96207c0f5c655970bc764e3152cfc02d
mohammad <mshayganfar@wpi.edu>
2015-08-06 10:34:42 -0400

    Added simplegraph visualization.

--- src/AppraisalMechanism.java
+++ src/AppraisalMechanism.java
@@ -1,5 +1,12 @@
+import java.awt.Dimension;
+
+import javax.swing.JFrame;
+
+import edu.uci.ics.jung.algorithms.layout.CircleLayout;
+import edu.uci.ics.jung.algorithms.layout.Layout;
 import edu.uci.ics.jung.graph.*;
 import edu.uci.ics.jung.graph.util.EdgeType;
+import edu.uci.ics.jung.visualization.BasicVisualizationServer;
 import jess.*;
 
 /*
@@ -38,12 +45,24 @@ public class AppraisalMechanism {
 			
 		    JessEngine.eval("(facts)");
 		    
-		    Graph<Fact, String> graph = new SparseGraph<Fact, String>();
+		    Graph<Fact, String> graph = new DirectedSparseGraph<Fact, String>();
 		    graph.addVertex(fact1);
 		    graph.addVertex(fact2);
 		    graph.addEdge("Edge-1", fact1, fact2, EdgeType.DIRECTED);
 		    
 		    System.out.println("The graph = " + graph.toString());
+		    
+//		    SimpleGraphView sgv = new SimpleGraphView();
+		    Layout<Fact, String> layout = new CircleLayout<>(graph);
+		    layout.setSize(new Dimension(300,300));
+		    BasicVisualizationServer<Fact, String> vv = new BasicVisualizationServer<Fact, String>(layout);
+		    vv.setPreferredSize(new Dimension(350, 350));
+		    
+		    JFrame frame = new JFrame("My Graph View");
+		    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		    frame.getContentPane().add(vv);
+		    frame.pack();
+		    frame.setVisible(true);
 		} catch (JessException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
