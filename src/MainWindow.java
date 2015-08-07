685bcd8d28f2d7eff84c6dc5ba51ab4714f34825
mohammad <mshayganfar@wpi.edu>
2015-08-07 10:09:12 -0400

    Added MigLayout-based GUI.

--- src/MainWindow.java
+++ src/MainWindow.java
@@ -1,15 +1,23 @@
-import java.awt.BorderLayout;
 import java.awt.Dimension;
 import java.awt.EventQueue;
 import java.awt.event.ActionListener;
 
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
+import net.miginfocom.swing.MigLayout;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.JButton;
 
 
 public class MainWindow {
 
 	private JFrame frame;
+	private JLabel lblMentalStateTypes;
+	private JLabel lblNewMentalState;
+	private JTextField txtNewMentalState;
+	private JComboBox cmbMentalStateTypes;
+	private JButton btnAdd;
 
 	/**
 	 * Launch the application.
@@ -41,14 +49,28 @@ public class MainWindow {
 		String[] strMentalStates = { " " , "Beilef", "Intention", "Motive", "Goal", "Emotion" };
 		
 		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 300);
+		frame.setBounds(100, 100, 450, 150);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(new MigLayout("", "[][grow]", "[][][grow]"));
 		
-		JComboBox cmbMentalStates = new JComboBox(strMentalStates);
-		cmbMentalStates.setSelectedIndex(0);
-		cmbMentalStates.setPreferredSize(new Dimension(10,25));
-		cmbMentalStates.addActionListener((ActionListener) this);
-		frame.getContentPane().add(cmbMentalStates, BorderLayout.NORTH);
+		lblMentalStateTypes = new JLabel("Mental State Types:");
+		lblNewMentalState   = new JLabel("New Mental State:");
+		
+		cmbMentalStateTypes = new JComboBox(strMentalStates);
+		cmbMentalStateTypes.setSelectedIndex(0);
+		cmbMentalStateTypes.setPreferredSize(new Dimension(10,25));
+		cmbMentalStateTypes.addActionListener((ActionListener) this);
+		
+		txtNewMentalState = new JTextField();
+		txtNewMentalState.setColumns(10);
+		
+		btnAdd = new JButton("Add");
+		
+		frame.getContentPane().add(lblMentalStateTypes, "cell 0 0");
+		frame.getContentPane().add(cmbMentalStateTypes, "cell 1 0,width 120!");
+		frame.getContentPane().add(lblNewMentalState, "cell 0 1");
+		frame.getContentPane().add(txtNewMentalState, "cell 1 1,growx");
+		frame.getContentPane().add(btnAdd, "cell 1 2,align right,width 80!");
 	}
 
 }
